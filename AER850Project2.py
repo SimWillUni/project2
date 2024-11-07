e0fc46cfdfac6b7302760cba03f7ca6c2da358cb
SimWillUni <simon.willemsma@torontomu.ca>
2024-11-07 00:20:34 -0500

    Created model
    Added many layers to a keras sequential model

--- AER850Project2.py
+++ AER850Project2.py
@@ -1,6 +1,10 @@
 import keras
+from keras import layers, models
 from keras.preprocessing import image_dataset_from_directory
-from keras.layers import Rescaling, RandomZoom, RandomTranslation, RandomFlip
+
+""" Step 1: Data Processing """
+
+# Generators
 
 trainset = image_dataset_from_directory(
     'data/train',
@@ -16,13 +20,37 @@ validset = image_dataset_from_directory(
     label_mode='categorical'
 )
 
-augment = keras.Sequential([
-    Rescaling(1./255),
-    RandomZoom(0.1),
-    RandomTranslation(0.1,0.1),
-    RandomFlip("horizontal")
+# Data Augmentation
+
+augment = models.Sequential([
+    layers.Rescaling(1./255),
+    layers.RandomZoom(0.1),
+    layers.RandomTranslation(0.1,0.1),
+    layers.RandomFlip("horizontal")
 ])
 
 trainset = trainset.map(lambda x, y: (augment(x, training=True), y))
 
-validset = validset.map(lambda x, y: (Rescaling(1./255)(x), y))
+validset = validset.map(lambda x, y: (layers.Rescaling(1./255)(x), y))
+
+""" Step 2: Neural Network Architecture Design """
+
+mymodel = models.Sequential([
+
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(500, 500, 3)),
+    layers.MaxPooling2D((2, 2)),
+
+
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+
+
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+
+
+    layers.Flatten(), # turns 2D into 1D data
+    layers.Dense(64, activation='relu'),
+    layers.Dropout(0.5), # drop half of the neurons
+    layers.Dense(3, activation='relu')]
+)
