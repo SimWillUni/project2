221679d825607d62c20fffdd80e9f3b29f41372a
SimWillUni <simon.willemsma@torontomu.ca>
2024-11-10 18:32:50 -0500

    finished step 5
    made a function which takes an image path and returns an image similar to that desired in the project manual

--- testing.py
+++ testing.py
@@ -12,3 +12,28 @@ classes = ["Crack", "Missing Head", "Paint Off"]
 #data/test/crack/test_paintoff.jpg
 #data/test/crack/test_missinghead.jpg
 
+# func to predict the class and display the image
+def display_prediction(img_path):
+    img = image.load_img(img_path, target_size=(500, 500))
+    img_array = image.img_to_array(img) / 255.0  # normalizing the img data
+    img_array = np.expand_dims(img_array, axis=0)
+
+    # class prediction
+    predictions = mymodel.predict(img_array)[0]
+    predicted_class = classes[np.argmax(predictions)]
+    
+    plt.imshow(img)
+    plt.axis('off')
+    
+    # putting the text on the image
+    text = "\n".join([f"{classes[i]}: {predictions[i]:.2f}" for i in range(len(classes))])
+    plt.text(
+        10, 450, text, color="green", fontsize=16, 
+        bbox=dict(facecolor="white", alpha=0.5)
+    )
+    plt.title(f"Predicted: {predicted_class}")
+    plt.show()
+
+display_prediction("data/test/crack/test_crack.jpg")
+display_prediction("data/test/paint-off/test_paintoff.jpg")
+display_prediction("data/test/missing-head/test_missinghead.jpg")
